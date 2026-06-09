d9d5f92cd68df2afcafa4e8e0cd682f7c59d4138
ilovefood7871 <mohammediscoolwhynotlol@gmail.com>
2026-06-09 16:40:37 -0700

    add copyright and credits

--- executor/v2executor/Bootstrapper.cs
+++ executor/v2executor/Bootstrapper.cs
@@ -1,3 +1,6 @@
+// © BNE Softworks (github.com/newilf7871)
+// chrome webview tabs from https://github.com/adamschwartz/chrome-tabs credit to him
+// licensed under GNU GPLv3 (see LICENSE)
 using System;
 using System.IO;
 using System.IO.Compression;
